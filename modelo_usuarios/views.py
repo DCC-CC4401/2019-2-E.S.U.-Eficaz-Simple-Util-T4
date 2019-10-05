1fc96255e0181b1b2a82776e13f988e7eea06f1e
Jalbornozv <julio.albornoz@ug.uchile.cl>
2019-10-04 23:44:39 -0400

    Configured django to display the logged user Interface 	- Added a placeholder view to render the page 	- Allowed development through localhost/127.0.0.1 	- Added static Urls 	- Configured urls.py 	- Added static tags to the templates to display the css/js properly

--- modelo_usuarios/views.py
+++ modelo_usuarios/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_user(request):
+	"""
+	Landing Page for logged accounts //Placeholder
+	"""
+	return render(request, "LandingPage_logeado.html")
+	
--- proyecto_ESU/settings.py
+++ proyecto_ESU/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aas^!&j!p5gmpar3bcx#h184q%)vk)01=^e-%xa19&eq^pyd#b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/bootstrap"),
+    os.path.join(BASE_DIR, "static/Prototypes"),
+]
