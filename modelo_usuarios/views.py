e55a171ab2b042bc93177417bc2735bc1ef4d73a
Victor Faraggi <sfaragg@gmail.com>
2019-12-13 15:07:35 -0300

    password view

--- modelo_usuarios/views.py
+++ modelo_usuarios/views.py
@@ -1,17 +1,14 @@
+from django.contrib.auth import login, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.shortcuts import render
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from django.shortcuts import redirect
-from .models import Profile
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.staticfiles.templatetags.staticfiles import static
-from .forms import LogSession
+from django.shortcuts import redirect
+from django.shortcuts import render
 
 # Create your views here.
 from modelo_usuarios import forms
 from modelo_usuarios.models import Profile
+from .forms import LogSession
 
 
 def home_unsigned(request):
@@ -101,10 +98,23 @@ def update_profile_photo(request):
     if request.method == "POST":
         form = forms.PhotoForm(request.POST, request.FILES)
         if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = request.user
+            profile = Profile.objects.get(email=request.user)
+            profile.profile_photo = request.FILES['imagefile']
             profile.save()
             return redirect("UserProfile.html")
     else:
         form = forms.PhotoForm()
+    return render(request, 'UserProfile.html', {'form': form})
+
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+    else:
+        form = PasswordChangeForm(request.user)
+
     return render(request, 'UserProfile.html', {'form': form})
